1ec2243efe50554491dc42d4b2cf4aae820a9b9a
Ryan <ryankohjingjie@gmail.com>
2026-01-26 11:03:30 +0800

    New codes

--- processer.py
+++ processer.py
@@ -1,7 +1,7 @@
 import os
-#import json
+import json
 import PyPDF2
-# import google.generativeai as genai
+import google.generativeai as genai
 #from dotenv import load_dotenv
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
@@ -30,6 +30,56 @@ class TextProcessor:
     def get_chunk(self, full_text):
         chunk = self.text_splitter.split_text(full_text)
         return chunk
+    
+    '''Generate Script'''
+    def generate_script(chunks):
+        full_script = []
+        previous_text = '' #Baton
+
+        for i in range(chunks):
+
+            #Prompt
+            prompt = f'''
+            ### ROLE
+            You are a professional podcast producer. Your job is to convert study notes into a
+            strictly structured JSON dialouge script. Assume that the listener has NO prior knowledge 
+            of this topic, try to break down concepts into simpler chunks.
+
+            ### CHARACTERS INVOLVED
+            1. **Alex (Host): **A curious, energetic, asks clarifying questions kind of student.
+            2. **John (Expert): **A calm, knowledgeable, uses everyday language and simple analogies to explain complex topics.
+
+            ### INPUT DATA
+            Here is the raw text to discuss: '{chunks}'
+
+            ### CONTEXT (PREVIOUS CONVERSATION)
+            The podcast is already in progess. Here is the last thing that was said: '{previous_text}'
+
+            ### INSTRUCTIONS
+            1. Continue the conversation naturally from the CONTEXT. (no 'Welcome back')
+            2. If CONTEXT is empty, start with a catchy introduction.
+            3. Explain the INPUT DATA clearly through dialouge. Keep iteractions short (2-3 sentence per turn).
+            4. End the segment natuarlly, but do not say 'Goodbye' or 'In summary' unless the text explicitly ends.
+
+            ### OUTPUT FORMAT (CRITICAL)
+            Output ONLY valid JSON. Do not use Markdown.
+            Follow this exact schema:
+            [
+                {{'speaker': 'Alex', 'text': 'Wait, so how does that work?'}}
+                {{'speaker': 'John', 'text': 'Think of it like a battery...'}}           
+            ]
+            '''
+        
+            #Generate prompt with Gemini
+            response = genai.generate(prompt)
+            current_segment = json.loads(response)
+
+            #Append it to the Script
+            full_script.extend(current_segment)
+
+            #Updating of Baton
+            #Grab the last few items from current_segment to show the next iteration
+            previous_text = str(current_segment[-3:])
 
 
 if __name__ == '__main__':
